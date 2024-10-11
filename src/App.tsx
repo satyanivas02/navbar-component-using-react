8d7de74cd2ef3cc23c9b9cd43f62e6aca9df13bd
satyanivas02 <satyanivas.miryala@zemosolabs.com>
2024-10-11 15:46:27 +0530

    cleaned app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import {ThemeProvider} from '@mui/material';
-import theme from './theme/theme';
-import NavbarTemplate from './components/template/Navbar';
 
 function App(){
   return(
-    <ThemeProvider theme={theme}>
-      <NavbarTemplate/>
-    </ThemeProvider>
+    <></>
   )
 }
 export default App;
